cdf6d9cfaf2830583cb34881ae7df547eeb52481
Lu1ro <pro302pro302@gmail.com>
2025-12-19 23:10:05 +0200

    Fix build errors and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Config options here */
   eslint: {
-    // Ігнорувати помилки лінтера під час білду (щоб Vercel не сварився)
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ігнорувати помилки типів (ми знаємо, що код робочий)
     ignoreBuildErrors: true,
   },
 };
