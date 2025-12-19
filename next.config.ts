675951345516d3712aeaa259f084901548e0ce30
Lu1ro <pro302pro302@gmail.com>
2025-12-19 23:14:24 +0200

    Install missing libraries and fix build settings

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
+    // Це дозволить Vercel ігнорувати помилки стилю при збірці
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Це дозволить ігнорувати технічні помилки типів
     ignoreBuildErrors: true,
   },
 };
