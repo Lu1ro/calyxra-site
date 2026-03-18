d073220e295b10fbfba88311e583f41c320564f5
Lu1ro <pro302pro302@gmail.com>
2026-03-18 09:00:50 +0200

    fix: force prisma generate in build, add serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), '@prisma/client'];
-    }
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: '/demo',
-        destination: '/dashboards',
-        permanent: true,
-      },
-    ];
-  },
+  serverExternalPackages: ['@prisma/client', 'bcryptjs'],
 };
 
 export default nextConfig;
